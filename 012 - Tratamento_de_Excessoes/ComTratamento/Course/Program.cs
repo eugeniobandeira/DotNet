ae5be7711da4259b1e34acac48c613db4127ca2a
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-09-11 07:58:07 -0300

    +Tratamento de Excessão

---
+++ 012 - Tratamento_de_Excessoes/ComTratamento/Course/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using Course.Entities;
+using Course.Entities.Exceptions;
+using System;
+using System.Linq.Expressions;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            Console.WriteLine("Enter account data");
+            Console.Write("Room: ");
+            int room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            Reservation booking = new Reservation(room, checkin, checkout);
+            booking.Duration();
+
+            Console.WriteLine();
+            Console.WriteLine("Reservation: " + booking);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the data to update the reservation:");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            checkout = DateTime.Parse(Console.ReadLine());
+            booking.UpdateDates(checkin, checkout);
+
+            Console.WriteLine("Reservation: " + booking);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Erro in reservation: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+         
+        }
+
+    }
+}
+
---
+++ 012 - Tratamento_de_Excessoes/SemTratamento/Course/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Course.Entities;
+using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Room: ");
+            int room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if(checkout < checkin)
+            {
+                Console.WriteLine("Error in reservation: check-out must be after check-in");
+            }
+            else
+            {
+                Reservation booking = new Reservation(room, checkin, checkout);
+                booking.Duration();
+                Console.WriteLine("Reservation: " + booking);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter the data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if(checkin < now || checkout < now) {
+                    Console.WriteLine("Error in reservation: check-in date must be for future.");
+                } else if (checkin < now || checkout < now)
+                {
+                    Console.WriteLine("Error in reservation: check-out must be after check-in.");
+                } else
+                {
+                    booking.UpdateDates(checkin, checkout);
+                    Console.WriteLine($"{booking}");
+                }
+
+            }
+            
+        }
+    }
+}
